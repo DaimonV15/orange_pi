d491a7a33ec5d483d23c7346416d18f55881b406
Dmitry_Dimitrov <dimitrovdimad@mail.ru>
2022-06-02 14:47:40 +0300

    change jdk 17->11

--- src/Face_Identification_Diplom/Main.java
+++ src/Face_Identification_Diplom/Main.java
@@ -1,12 +1,7 @@
 package src.Face_Identification_Diplom;
 
 import org.opencv.core.Core;
-import org.opencv.core.Mat;
-import org.opencv.highgui.HighGui;
-import org.opencv.imgcodecs.Imgcodecs;
-
 import java.io.IOException;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) throws IOException {
